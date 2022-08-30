984453311ad002a496da0b08224d26d0225f5ef2
Ignacy2012 <107277585+Ignacy2012@users.noreply.github.com>
2022-08-30 22:23:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log("hi this is my first consol project")
+
+
+var firstname="Ranbir";
+
+
+var lastname="Kapoor";
+
+
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+
+var replace=fullname.replace("Kapoor"," Kapoor");
+console.log(replace);
+
+var a=10;
+var b=5;
+
+var add = a + b ;
+console.log(add);
+
+var squareroot=Math.sqrt(5);
+console.log(squareroot);
+
+console.log(a - b);
+
+console.log(b * a);
